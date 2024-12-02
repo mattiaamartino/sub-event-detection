ae2f13472f3c4febb5fdfaae8b6f518149197a90
alessandropranzo <alepranzo@gmail.com>
2024-12-02 16:15:38 +0100

    Update the structure

--- utils/simple_embedding.py
+++ utils/simple_embedding.py
@@ -10,6 +10,17 @@ base_model = AutoModel.from_pretrained("distilbert-base-uncased").to(device)
 
 
 def embed_text(text, embedding_technique="mean"):
+    """
+    Embeds the given text using a specified embedding technique.
+    Args:
+        text Iterable(str): The input text to be embedded. Can be a list of texts.
+        embedding_technique (str, optional): The technique to use for embedding. 
+            Options are "mean" for mean pooling and "cls" for using the [CLS] token embedding. 
+            Defaults to "mean".
+    Returns:
+        torch.Tensor: The embedded representation of the input text (bs, 768).
+    """
+
     inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True).to(device)
     input_ids = inputs["input_ids"]
     
